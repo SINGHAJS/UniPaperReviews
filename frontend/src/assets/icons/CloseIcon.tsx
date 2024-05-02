9d29d5a27caec4ae12610e0a7370332b3a6cf6c6
Ajit Singh <ajitsingh@Ajits-MacBook-Pro.local>
2024-05-02 17:25:43 +1200

    add bar and close icons

---
+++ frontend/src/assets/icons/CloseIcon.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+export default function CloseIcon() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      strokeWidth={2}
+      stroke="white"
+      className="w-8 h-8"
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M6 18 18 6M6 6l12 12"
+      />
+    </svg>
+  );
+}

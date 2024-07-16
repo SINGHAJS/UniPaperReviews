2c4d93afdbcc4167ad49f64a95b0a7f8fde4433c
Ajit Singh <ajitisngh@Ajits-MacBook-Pro.local>
2024-07-17 00:11:13 +1200

    set width of AppHeader component

--- frontend/src/shared/components/Navigation/AppHeader.tsx
+++ frontend/src/shared/components/Navigation/AppHeader.tsx
@@ -1,12 +1,12 @@
 import { ReactNode } from "react";
 
-type TAppHeader = {
+export type TAppHeader = {
   children: ReactNode;
 };
 
 export const AppHeader = ({ children }: TAppHeader) => {
   return (
-    <div className="bg-white border-b-2 border-gray-300 flex justify-between items-center p-4">
+    <div className="bg-white border-b-2 border-gray-300 flex justify-between items-center p-4 w-full">
       <h1 className="font-bold">UniPaperReviews</h1>
       {children}
     </div>

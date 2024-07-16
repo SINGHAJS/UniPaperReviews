1da5c65486b205575cc4cba1756b0845ca67ea50
Ajit Singh <ajitisngh@Ajits-MacBook-Pro.local>
2024-07-17 00:13:31 +1200

    add a div as the wrapper element

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,12 +4,12 @@ import { Reviews } from "./reviews/pages/Reviews";
 
 function App() {
   return (
-    <>
+    <div className="w-full flex flex-col items-center">
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/paper-reviews" element={<Reviews />} />
       </Routes>
-    </>
+    </div>
   );
 }
 

377ae2f391a57a74990d3e77570bd848f07232a6
Ajit Singh <ajitisngh@Ajits-MacBook-Pro.local>
2024-07-17 00:11:59 +1200

    add function to render the Review components from a list of objects

--- frontend/src/reviews/pages/Reviews.tsx
+++ frontend/src/reviews/pages/Reviews.tsx
@@ -1,10 +1,42 @@
 import { AppNavigation } from "../../shared/components/Navigation/AppNavigation";
+// import {TReview, Review} from "../components/Review.tsx";
+import { TReview, Review } from "../components/Review.tsx";
+import { NoPaperAvailable } from "../../shared/ErrorMessages/NoPaperAvailable.tsx";
 
 export const Reviews = () => {
+  const reviews: TReview[] = [
+    {
+      paper: "COMP101",
+      university: "Auckland University of Technology (AUT)",
+      datePosted: "16/07/24",
+      degree: "Bachelors of Engineering (Honours) Software",
+    },
+    {
+      paper: "ENGE111",
+      university: "Auckland University of Technology (AUT)",
+      datePosted: "16/07/24",
+      degree: "Bachelors of Engineering (Honours) Software",
+    },
+  ];
+
   return (
     <>
       <AppNavigation />
-      <h1> Reviews </h1>
+      <div className="w-4/5 bg-white mt-4 p-8 rounded-xl border-4 border-black flex flex-col">
+        <div>
+          {reviews.length === 0 && <NoPaperAvailable />}
+          {reviews.map((review) => {
+            return (
+              <Review
+                paper={review.paper}
+                datePosted={review.datePosted}
+                university={review.university}
+                degree={review.degree}
+              />
+            );
+          })}
+        </div>
+      </div>
     </>
   );
 };

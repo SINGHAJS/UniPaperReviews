43f5399a392f3ce817f86cbc023749b81dcb9d45
Ajit Singh <singhajs@Ajits-MacBook-Pro.local>
2024-12-26 15:16:32 +1300

    add onClick

--- frontend/src/shared/uiElements/CButton.tsx
+++ frontend/src/shared/uiElements/CButton.tsx
@@ -4,15 +4,17 @@ import { twMerge } from "tailwind-merge";
 type TCButton = {
   children: ReactNode;
   className?: string;
-};
+  onClick?: () => void;
+} ;
 
-export const CButton = ({ children, className, ...props }: TCButton) => {
+export const CButton: React.FC<TCButton> = ({ children, className, onClick, ...props }) => {
   return (
     <button
       className={twMerge(
         "px-6 py-2 w-48 text-sm font-semibold border-2 rounded transition ease-in duration-500 text-black bg-white hover:text-white hover:bg-black",
         className
       )}
+      onClick={onClick}
       {...props}
     >
       {children}
